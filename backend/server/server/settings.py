0b5bac0e2d62f0503241e01ff137e1850a63114c
SatyaSiddharthDash <dash.sathyasiddharth@gmail.com>
2020-08-29 20:10:02 +0530

    remove keys

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y%ng*=u-y%erwjw%js-m8ex1g(11je(u1j8uit1t-v3h##%sug'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
